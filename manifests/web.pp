b30f83c4328a7dc7904712827ecaa814696ab6ec
Simon Josi <simon.josi@swisstxt.ch>
2013-03-08 17:16:58 +0100

    install passenger for opt-ruby-1.9.3 directly for now

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,8 +1,12 @@
 class graylog2::web {
   include apache
 
-  class{'passenger':
-    base => 'opt-ruby-1.9.3',
+  #class{'passenger':
+  #  base => 'opt-ruby-1.9.3',
+  #}
+  # TODO => do this via passenger module
+  package{'opt-ruby-1.9.3-mod_passenger':
+    ensure => present,
   }
   package{'graylog2-web-interface':
     ensure => present,
